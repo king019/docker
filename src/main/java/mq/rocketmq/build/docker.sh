4e9b0534bbf8994ddce0f9391cb77c65a31aec18
k <k@qq.com>
2019-03-09 01:54:11 +0800

    1

--- src/main/java/mq/rocketmq/build/docker.sh
+++ src/main/java/mq/rocketmq/build/docker.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 set -x
-tail -f /docker.sh
 
+mkdir -p /opt/soft
+
+cd /opt/soft
+
+cd /opt/soft/rocketmq/rocketmq-4.4
+
+nohup sh bin/mqnamesrv &
+sleep 5
+nohup sh bin/mqbroker -n 127.0.0.1:9876 &
+sleep 5
+cd /opt/soft/rocketmq/rocketmq-console
+nohup mvn spring-boot:run &
+tail -f /docker.sh

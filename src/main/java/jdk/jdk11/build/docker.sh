69c1ae0c5ef202419c3709e033814c20345c49b0
k <k@qq.com>
2019-09-06 10:58:33 +0800

    1

--- src/main/java/jdk/jdk11/build/docker.sh
+++ src/main/java/jdk/jdk11/build/docker.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -x
 
+cd /soft/
+tar -xzf openjdk-11_linux-x64_bin.tar.gz
+mv jdk-11 jdk
+
+echo 'export JAVA_HOME=/soft/jdk' >> /etc/profile
+echo 'export PATH=$JAVA_HOME/bin:$PATH' >> /etc/profile
+
+
 source /etc/profile
 
 tail -f /docker.sh
--- src/main/java/jdk/jdk9/build/docker.sh
+++ src/main/java/jdk/jdk9/build/docker.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -x
 
+cd /soft/
+tar -xzf openjdk-9_linux-x64_bin.tar.gz
+mv jdk-9 jdk
+
+echo 'export JAVA_HOME=/soft/jdk' >> /etc/profile
+echo 'export PATH=$JAVA_HOME/bin:$PATH' >> /etc/profile
+
+
 source /etc/profile
 
 tail -f /docker.sh

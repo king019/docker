a1a6ac6dc64afa8403a4564c8d04480afb5a19e2
k <k@qq.com>
2020-04-12 12:34:24 +0800

    1

--- src/main/resources/arm/jdk/jdk10/build/docker.sh
+++ src/main/resources/arm/jdk/jdk10/build/docker.sh
@@ -6,8 +6,8 @@ tar -xzf apache-tomcat-9.0.24.tar.gz
 
 
 cd /op/soft/
-tar -xzf openjdk-10_linux-x64_bin.tar.gz
-mv jdk-10 jdk
+tar -xzf OpenJDK10U-jdk_arm_linux_hotspot_10.0.2_13.tar.gz
+mv jdk-10.0.2+13.1 jdk
 
 echo 'export JAVA_HOME=/opt/soft/jdk' >> /etc/profile
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >> /etc/profile
--- src/main/resources/arm/jdk/jdk11/build/docker.sh
+++ src/main/resources/arm/jdk/jdk11/build/docker.sh
@@ -6,8 +6,8 @@ tar -xzf apache-tomcat-9.0.24.tar.gz
 
 
 cd /opt/soft/
-tar -xzf openjdk-11_linux-x64_bin.tar.gz
-mv jdk-11 jdk
+tar -xzf OpenJDK11U-jdk_arm_linux_hotspot_2020-04-09-17-31.tar.gz
+mv jdk-11.0.7+9 jdk
 
 echo 'export JAVA_HOME=/opt/soft/jdk' >> /etc/profile
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >> /etc/profile

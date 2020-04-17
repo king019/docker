91e36aca396a1cb902171a42e160d2bc1053ea8b
k <k@qq.com>
2020-04-18 00:10:20 +0800

    1

--- src/main/resources/common/jdk/jdk11/build/docker.sh
+++ src/main/resources/common/jdk/jdk11/build/docker.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 set -x
 
-cd /opt/soft/
-tar -xzf apache-tomcat-9.0.24.tar.gz
-
-
-#cd /opt/soft/
-#tar -xzf openjdk-11_linux-x64_bin.tar.gz
-#mv jdk-11 jdk
-
-#echo 'export JAVA_HOME=/opt/soft/jdk' >> /etc/profile
-#echo 'export PATH=$JAVA_HOME/bin:$PATH' >> /etc/profile
-
-
-source /etc/profile
-
 tail -f /docker.sh
--- src/main/resources/common/jdk/jdk8/build/docker.sh
+++ src/main/resources/common/jdk/jdk8/build/docker.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -x
 
-cd /opt/soft/
-tar -xzf apache-tomcat-9.0.24.tar.gz
-
 tail -f /docker.sh

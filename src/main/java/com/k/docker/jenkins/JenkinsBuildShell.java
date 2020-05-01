09cca5ed42161a5a42755c2879a96ee3f0adf8ab
k <k@qq.com>
2020-05-01 11:25:48 +0800

    1

--- src/main/java/com/k/docker/jenkins/JenkinsBuildShell.java
+++ src/main/java/com/k/docker/jenkins/JenkinsBuildShell.java
@@ -73,6 +73,7 @@ public class JenkinsBuildShell {
         models = models.stream().filter(model -> DockerPlatformEnum.ADM64.equals(model.getPlatform())).map(model -> {
             model.setPlatforms(ImmutableSet.of());
             model.setPlatform(DockerPlatformEnum.NO_SUFFIX);
+            model.setIndex(1);
             return model;
         }).filter(model -> {
             Set<DockerRegionEnum> ignoreRegions = model.getIgnoreRegions();

d21cd9d888d59bae3b79cc1eb742b2b6133d0211
k <k@qq.com>
2020-06-13 21:24:38 +0800

    1

--- src/main/resources/common/alibaba/sentinel/build/docker.sh
+++ src/main/resources/common/alibaba/sentinel/build/docker.sh
@@ -2,5 +2,5 @@
 set -x
 source /etc/profile
 cd /root/tools
-java -Dserver.port=8080 -Dcsp.sentinel.dashboard.server=localhost:8080 -Dproject.name=sentinel-dashboard -jar target/sentinel-dashboard.jar
+java -Dserver.port=8080 -Dcsp.sentinel.dashboard.server=localhost:8080 -Dproject.name=sentinel-dashboard -jar sentinel-dashboard.jar
 tail -f /docker.sh

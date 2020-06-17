0c0833e539576cea3c116a7c14d068819a5977d5
k <k@qq.com>
2020-06-18 02:23:14 +0800

    1

--- src/main/resources/common/alibaba/dubbo/consumer/build/docker.sh
+++ src/main/resources/common/alibaba/dubbo/consumer/build/docker.sh
@@ -2,6 +2,6 @@
 set -x
 source /etc/profile
 cd /root/tools
-java $JAVA_OPTS -jar  fw_rpc_dubbo_anno_api-1.0-SNAPSHOT.jar >>log.txt
+nohup java $JAVA_OPTS -jar  fw_rpc_dubbo_anno_api-1.0-SNAPSHOT.jar >>log.txt &
 
 tail -f /docker.sh

d58e07a3e8d9cfad3fd3b66eed78e1e1ed580fe5
k <k@qq.com>
2020-07-01 20:50:34 +0800

    1

--- src/main/resources/common/alibaba/rocketmq/rocketmq-console/build/docker.sh
+++ src/main/resources/common/alibaba/rocketmq/rocketmq-console/build/docker.sh
@@ -2,6 +2,5 @@
 set -x
 source /etc/profile
 cd /root/tools
-nohup java $JAVA_OPTS -Dadmin.registry.address=zookeeper://zookeeper:2181 -Dadmin.config-center=zookeeper://zookeeper:2181 -Dadmin.metadata-report.address=zookeeper://zookeeper:2181 -jar  dubbo-admin-0.2.0.jar &
 
 tail -f /docker.sh
--- src/main/resources/common/alibaba/rocketmq/rocketmq/build/docker.sh
+++ src/main/resources/common/alibaba/rocketmq/rocketmq/build/docker.sh
@@ -2,6 +2,5 @@
 set -x
 source /etc/profile
 cd /root/soft
-nohup java $JAVA_OPTS -jar  /root/tools/fw_rpc_dubbo_anno_api-1.0-SNAPSHOT.jar >>log.txt &
 
 tail -f /docker.sh

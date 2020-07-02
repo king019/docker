277b7204978a541bf774fb4e663971d7f792144c
k <k@qq.com>
2020-07-03 01:41:37 +0800

    1

--- src/main/resources/common/alibaba/rocketmq/rocketmq/build/docker.sh
+++ src/main/resources/common/alibaba/rocketmq/rocketmq/build/docker.sh
@@ -4,5 +4,5 @@ source /etc/profile
 cd /root/tools/rocketmq
 nohup sh bin/mqnamesrv &
 sleep 20
-nohup sh bin/mqbroker -n 127.0.0.1:9876 &
+nohup sh bin/mqbroker -n rocketmq:9876 &
 tail -f /docker.sh

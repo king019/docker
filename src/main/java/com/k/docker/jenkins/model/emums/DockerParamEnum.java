9e55bee47c11aa9b378a62c3b73aaef3e18d23c4
k <k@qq.com>
2020-07-09 23:16:05 +0800

    1

--- src/main/java/com/k/docker/jenkins/model/emums/DockerParamEnum.java
+++ src/main/java/com/k/docker/jenkins/model/emums/DockerParamEnum.java
@@ -14,7 +14,7 @@ public enum DockerParamEnum {
     INCLUDE("in",""),
     REPLACE("rep","false"),
     PUSH("push","true"),
-    IN_DOCKER("indk","false"),
+    IN_DOCKER("dk","false"),
      EXCLUDE  ("ex","");
     private String cmd;
     private String def;

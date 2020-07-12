7c88c887ecddc869cabd4a215648cd4a9432855f
k <k@qq.com>
2020-07-12 23:07:30 +0800

    1

--- src/main/resources/build/common/tools/linux/ctrip/apollo/protal/build/docker.sh
+++ src/main/resources/build/common/tools/linux/ctrip/apollo/protal/build/docker.sh
@@ -3,6 +3,9 @@ set -x
 
 
 source /etc/profile
+
+cd /root/tools/apollo-portal/config
+echo 'dev.meta=$DEV_MATA' > apollo-env.properties
 cd /root/tools/apollo-portal/scripts
 nohup sh startup.sh >log.txt &
 

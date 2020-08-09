c5a62a5ea368a8a7b782ab1d2948aaab4a16b1a6
k <k@qq.com>
2020-08-09 23:49:33 +0800

    1

--- src/main/resources/build/common/tools/linux/special/alibaba/nacos/nacos-grpc-server/build/docker.sh
+++ src/main/resources/build/common/tools/linux/special/alibaba/nacos/nacos-grpc-server/build/docker.sh
@@ -3,6 +3,6 @@ set -x
 source /etc/profile
 echo 'notice'> /root/notice.txt
 cd /root/soft
-nohup java $JAVA_OPTS -jar  /root/tools/sshm_boot_cloud_alibaba_nacos_provider-1.0-SNAPSHOT.jar >>log.txt &
+nohup java $JAVA_OPTS -jar  /root/tools/sshm_boot_cloud_grpc_provider-1.0-SNAPSHOT.jar >>log.txt &
 
 tail -f /docker.sh

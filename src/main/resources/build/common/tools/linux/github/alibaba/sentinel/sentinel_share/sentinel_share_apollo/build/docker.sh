7cb3ac1f18f6f95735330d526d1aaa6f305c1847
k <k@qq.com>
2020-09-18 16:32:44 +0800

    1

--- src/main/resources/build/common/tools/linux/github/alibaba/sentinel/sentinel_share/sentinel_share_apollo/build/docker.sh
+++ src/main/resources/build/common/tools/linux/github/alibaba/sentinel/sentinel_share/sentinel_share_apollo/build/docker.sh
@@ -2,6 +2,6 @@
 set -x
 source /etc/profile
 cd /root/tools
-java  $JAVA_OPTS share_sentinel_apollo-1.0-SNAPSHOT.jar
+nohup java  $JAVA_OPTS -jar share_sentinel_apollo-1.0-SNAPSHOT.jar &
 sh /curlRun.sh
 tail -f /docker.sh

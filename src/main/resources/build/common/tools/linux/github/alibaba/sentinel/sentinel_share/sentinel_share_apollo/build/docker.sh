fe0dde3140dec9a6d76d0f3200906983485fd7cf
k <k@qq.com>
2020-09-18 19:49:17 +0800

    1

--- src/main/resources/build/common/tools/linux/github/alibaba/sentinel/sentinel_share/sentinel_share_apollo/build/docker.sh
+++ src/main/resources/build/common/tools/linux/github/alibaba/sentinel/sentinel_share/sentinel_share_apollo/build/docker.sh
@@ -3,7 +3,7 @@ set -x
 source /etc/profile
 cd /root/tools
 nohup java  $JAVA_OPTS -jar share_sentinel_apollo-1.0-SNAPSHOT.jar &
-for i in $( seq 1 30 )
+for i in $( seq 1 10 )
 do
 nohup sh /curlRun.sh $SERVER_PORT  &
 done

2657b0d748489e75a55168fb6f59b1397a06d04f
k <k@qq.com>
2020-09-19 00:05:42 +0800

    1

--- src/main/resources/build/common/tools/linux/github/alibaba/sentinel/sentinel_share/sentinel_share_apollo/build/curlRun.sh
+++ src/main/resources/build/common/tools/linux/github/alibaba/sentinel/sentinel_share/sentinel_share_apollo/build/curlRun.sh
@@ -3,4 +3,9 @@ while true
  do
    sleep $(($RANDOM%5))
    curl http://127.0.0.1:$1/index
+   curl http://127.0.0.1:$1/index
+   curl http://127.0.0.1:$1/index
+   curl http://127.0.0.1:$1/index
+   curl http://127.0.0.1:$1/index
+   curl http://127.0.0.1:$1/index
  done

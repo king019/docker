f38909ec4918acae47123c97f91baa6dd963ddd2
k <k@qq.com>
2020-09-22 01:53:38 +0800

    1

--- src/main/resources/build/common/tools/linux/github/docker/builddocker/build/docker.sh
+++ src/main/resources/build/common/tools/linux/github/docker/builddocker/build/docker.sh
@@ -18,5 +18,7 @@ mkdir -p /root/soft
 cd /root/soft
 git clone https://github.com/king019/docker.git
 cd docker
+source /etc/profile
+
 mvn compile exec:java -Dexec.mainClass="com.k.docker.jenkins.JenkinsBuildShell" -Dexec.args="ws=/root/soft/docker@thread=5@rep=false@push=true"
 sh ./target/x86_64_true.sh

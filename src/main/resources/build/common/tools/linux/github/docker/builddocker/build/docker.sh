12e115cb877721b40d9f72385ca96a714112c2a4
k <k@qq.com>
2020-09-22 01:18:42 +0800

    1

--- src/main/resources/build/common/tools/linux/github/docker/builddocker/build/docker.sh
+++ src/main/resources/build/common/tools/linux/github/docker/builddocker/build/docker.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -x
 
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 echo "$DOCKER_PASSWORD" | docker login -u "$ALIYUN_USERNAME" registry.cn-shanghai.aliyuncs.com --password-stdin
 echo "$DOCKER_PASSWORD" | docker login -u "$ALIYUN_USERNAME" registry.cn-huhehaote.aliyuncs.com --password-stdin

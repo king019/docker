419e3fea74a4134b43359fbe403ebe6dacccc206
k <k@qq.com>
2020-09-23 00:19:38 +0800

    1

--- src/main/resources/build/common/tools/linux/github/docker/builddocker/build/docker_run.sh
+++ src/main/resources/build/common/tools/linux/github/docker/builddocker/build/docker_run.sh
@@ -9,4 +9,4 @@ cd docker
 source /etc/profile
 
 mvn compile exec:java -Dexec.mainClass="com.k.docker.jenkins.JenkinsBuildShell" -Dexec.args="ws=/root/soft/docker@thread=5@rep=false@push=true"
-sh ./target/x86_64_true.sh
+sh ./target/x86_64_docker_true.sh

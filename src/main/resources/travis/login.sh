680dd95fe8619f221345ed3d2e111f6aa58614a8
k <k@qq.com>
2020-09-23 23:46:11 +0800

    1

--- src/main/resources/travis/login.sh
+++ src/main/resources/travis/login.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
+echo "$DOCKER_PASSWORD" | docker login -u "$ALIYUN_USERNAME" registry.cn-shanghai.aliyuncs.com --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$ALIYUN_USERNAME" registry.cn-huhehaote.aliyuncs.com --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$ALIYUN_USERNAME" registry.cn-shenzhen.aliyuncs.com --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$ALIYUN_USERNAME" registry.cn-hangzhou.aliyuncs.com --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$ALIYUN_USERNAME" registry.cn-beijing.aliyuncs.com --password-stdin

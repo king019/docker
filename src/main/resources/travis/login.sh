9db99de229c55feab5fe36394d85b5a37e53a4e0
king019 <king019@users.noreply.github.com>
2020-09-24 08:38:16 +0800

    Update login.sh

--- src/main/resources/travis/login.sh
+++ src/main/resources/travis/login.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-
-echo "$ALIYUN_PASSWORD" | docker login -u "$ALIYUN_USERNAME" registry.cn-shanghai.aliyuncs.com --password-stdin
-echo "$ALIYUN_PASSWORD" | docker login -u "$ALIYUN_USERNAME" registry.cn-huhehaote.aliyuncs.com --password-stdin
-echo "$ALIYUN_PASSWORD" | docker login -u "$ALIYUN_USERNAME" registry.cn-shenzhen.aliyuncs.com --password-stdin
-echo "$ALIYUN_PASSWORD" | docker login -u "$ALIYUN_USERNAME" registry.cn-hangzhou.aliyuncs.com --password-stdin
-echo "$ALIYUN_PASSWORD" | docker login -u "$ALIYUN_USERNAME" registry.cn-beijing.aliyuncs.com --password-stdin

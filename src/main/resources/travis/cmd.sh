6e9dd509c5e9551928d8133086df6b68acde9f1f
k <k@qq.com>
2020-10-16 01:08:01 +0800

    1

--- src/main/resources/travis/cmd.sh
+++ src/main/resources/travis/cmd.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -x
 mvn compile exec:java -Dexec.mainClass="com.k.docker.jenkins.JenkinsBuildShell" -Dexec.args="ws=/root/soft/docker@thread=5@rep=false@push=true@rg=dk"
-sh ./target/x86_64_docker_true.sh
+sh ./target/x86_64_aliyun_beijing_true.sh

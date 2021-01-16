3bd909814b58e5442b8e32ef813cb4e7261dafc0
k <k@qq.com>
2021-01-16 17:50:27 +0800

    1

---
+++ src/main/resources/build/common/tools/custom/spring/admin/build/git.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -x
+cd /opt/soft/version
+git clone https://github.com/king019/ali_fw_sshm.git
+cd ali_fw_sshm
+mvn clean package -DskipTests -Dmaven.javadoc.skip=true
+find . -name sshm_boot_cloud_admin_server-1.0-SNAPSHOT.jar|awk '{print "cp " $1  " /opt/soft/sshm_boot_cloud_admin_server-1.0-SNAPSHOT.jar"}'|sh
+mvn clean

ebb3bc786cc4a10a19f67555c785a10346d03586
k <k@qq.com>
2021-01-19 23:58:00 +0800

    1

--- src/main/resources/build/common/tools/custom/alibaba/sentinel/sentinel_dashbord/build/gitjar.sh
+++ src/main/resources/build/common/tools/custom/alibaba/sentinel/sentinel_dashbord/build/gitjar.sh
@@ -3,7 +3,7 @@ set -x
 cd /opt/soft/version
 git clone https://gitee.com/king019/Sentinel.git
 cd Sentinel
-mvn clean package -DskipTests -Dmaven.javadoc.skip=true
+mvn clean package -DskipTests -Dmaven.javadoc.skip=true -T 5
 find . -name sentinel-dashboard.jar|awk '{print "cp " $1  " /opt/soft/sentinel-dashboard.jar"}'|sh
 mvn clean
 rm -fr ~/.m2/repository

9002448c008d94a4c91f80c2b50a6cba5f01994c
k <k@qq.com>
2021-01-23 00:15:41 +0800

    1

--- src/main/resources/build/common/tools/custom/alibaba/dubbo/provider/build/gitjar.sh
+++ src/main/resources/build/common/tools/custom/alibaba/dubbo/provider/build/gitjar.sh
@@ -4,7 +4,7 @@ set -x
 cd /opt/soft/version
 git clone https://gitee.com/king019/ali_fw_fw.git
 cd ali_fw_fw
-mvn clean package -DskipTests -Dmaven.javadoc.skip=true -T 5
+mvn compile;mvn clean package -DskipTests -Dmaven.javadoc.skip=true -T 5
 find . -name fw_rpc_dubbo_anno_service-1.0-SNAPSHOT.jar|awk '{print "cp " $1  " /opt/soft/fw_rpc_dubbo_anno_service-1.0-SNAPSHOT.jar"}'|sh
 mvn clean
 rm -fr ~/.m2/repository

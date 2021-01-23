322d33c8228913579e9ca86f8d832733349d1c11
k <k@qq.com>
2021-01-23 14:43:17 +0800

    1

--- src/main/resources/build/common/tools/custom/alibaba/dubbo/provider/build/gitjar.sh
+++ src/main/resources/build/common/tools/custom/alibaba/dubbo/provider/build/gitjar.sh
@@ -3,8 +3,9 @@ set -x
 
 cd /opt/soft/version
 git clone https://gitee.com/king019/ali_fw_fw.git
+
 cd ali_fw_fw
-cd $(find . -name 'fw_rpc_dubbo_anno_service')
+cd $(find . -name 'fw_rpc_dubbo_anno')
 mvn clean install -T 5
 find . -name fw_rpc_dubbo_anno_service-1.0-SNAPSHOT.jar|awk '{print "cp " $1  " /opt/soft/fw_rpc_dubbo_anno_service-1.0-SNAPSHOT.jar"}'|sh
 mvn clean

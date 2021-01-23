e670cfd3481af017fce29fc2d4eba6625eb16a2a
k <k@qq.com>
2021-01-23 09:37:53 +0800

    1

--- src/main/resources/build/common/tools/custom/apache/zookeeper/zkui/build/gitjar.sh
+++ src/main/resources/build/common/tools/custom/apache/zookeeper/zkui/build/gitjar.sh
@@ -3,7 +3,7 @@ set -x
 cd /opt/soft/version
 git clone https://gitee.com/king019/zkui.git
 cd zkui
-mvn compile;mvn clean package -DskipTests -Dmaven.javadoc.skip=true -T 5
+mvn compile;mvn clean package -DskipTests -Dmaven.javadoc.skip=true
 find . -name zkui-2.0-SNAPSHOT-jar-with-dependencies.jar|awk '{print "cp " $1  " /opt/soft/zkui-2.0-SNAPSHOT-jar-with-dependencies.jar"}'|sh
 mvn clean
 rm -fr ~/.m2/repository

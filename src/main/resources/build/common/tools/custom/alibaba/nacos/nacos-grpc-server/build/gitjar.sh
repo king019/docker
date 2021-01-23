ab2bd911ee1256ee42a8acf39197020afbc18b1d
k <k@qq.com>
2021-01-23 10:22:30 +0800

    1

--- src/main/resources/build/common/tools/custom/alibaba/nacos/nacos-grpc-server/build/gitjar.sh
+++ src/main/resources/build/common/tools/custom/alibaba/nacos/nacos-grpc-server/build/gitjar.sh
@@ -3,7 +3,7 @@ set -x
 cd /opt/soft/version
 git clone https://gitee.com/king019/ali_fw_sshm.git
 cd ali_fw_sshm
-mvn compile;mvn clean package -DskipTests -Dmaven.javadoc.skip=true
+mvn compile;mvn clean package -DskipTests -Dmaven.javadoc.skip=true -X
 find . -name sshm_boot_cloud_grpc_provider-1.0-SNAPSHOT.jar|awk '{print "cp " $1  " /opt/soft/sshm_boot_cloud_grpc_provider-1.0-SNAPSHOT.jar"}'|sh
 mvn clean
 rm -fr ~/.m2/repository

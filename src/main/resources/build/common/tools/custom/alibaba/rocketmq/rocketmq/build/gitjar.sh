8b7c7bffbc999dfbd526f738017196bc56e2c212
k <k@qq.com>
2021-01-31 11:50:20 +0800

    1

---
+++ src/main/resources/build/common/tools/custom/alibaba/rocketmq/rocketmq/build/gitjar.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -x
+cd /opt/soft/version
+git clone https://e.coding.net/king019/github/rocketmq.git
+cd rocketmq
+
+mvn versions:set -DnewVersion=release
+mvn -Prelease-all -DskipTests clean install -T 5
+
+cp ./distribution/target/rocketmq-release.zip /opt/soft/rocketmq-release.zip
+
+mvn clean
+rm -fr ~/.m2/repository

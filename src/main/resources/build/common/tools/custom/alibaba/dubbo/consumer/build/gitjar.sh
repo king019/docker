342354e99db5f6965b528b2d5d42cd586f4cf2ac
k <k@qq.com>
2021-02-05 00:22:45 +0800

    1

--- src/main/resources/build/common/tools/custom/alibaba/dubbo/consumer/build/gitjar.sh
+++ src/main/resources/build/common/tools/custom/alibaba/dubbo/consumer/build/gitjar.sh
@@ -4,7 +4,7 @@ cd /opt/soft/version
 git clone https://e.coding.net/king019/github/ali_fw_fw.git
 cd ali_fw_fw
 cd $(find . -name 'fw_rpc_dubbo_anno_api')
-mvn clean install -DskipTests -Dmaven.javadoc.skip=true -Dgpg.skip -T 1
+mvn clean install -DskipTests -Dmaven.javadoc.skip=true -Dgpg.skip -T 5
 find . -name fw_rpc_dubbo_anno_api-1.0-SNAPSHOT.jar|awk '{print "cp " $1  " /opt/soft/fw_rpc_dubbo_anno_api-1.0-SNAPSHOT.jar"}'|sh
 mvn clean
 rm -fr ~/.m2/repository
--- src/main/resources/build/common/tools/custom/spring/admin/build/gitjar.sh
+++ src/main/resources/build/common/tools/custom/spring/admin/build/gitjar.sh
@@ -4,7 +4,7 @@ cd /opt/soft/version
 git clone https://e.coding.net/king019/github/ali_fw_sshm.git
 cd ali_fw_sshm
 cd $(find . -name 'sshm_boot_cloud_admin_server')
-mvn clean install -DskipTests -Dmaven.javadoc.skip=true -Dgpg.skip -T 1
+mvn clean install -DskipTests -Dmaven.javadoc.skip=true -Dgpg.skip -T 5
 find . -name sshm_boot_cloud_admin_server-1.0-SNAPSHOT.jar|awk '{print "cp " $1  " /opt/soft/sshm_boot_cloud_admin_server-1.0-SNAPSHOT.jar"}'|sh
 mvn clean
 rm -fr ~/.m2/repository

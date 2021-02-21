d549bd5154b9d8b25e50f19f995091ae48d996ec
k <k@qq.com>
2021-02-21 20:48:34 +0800

    1

--- src/main/resources/build/common/tools/custom/alibaba/rocketmq/rocketmq-console/build/docker.sh
+++ src/main/resources/build/common/tools/custom/alibaba/rocketmq/rocketmq-console/build/docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x
 source /etc/profile
-cd /root/tools
+cd /opt/soft
 nohup java $JAVA_OPTS -jar rocketmq-console-ng.jar >>log.txt &
 tail -f /docker.sh
---
+++ src/main/resources/build/common/tools/custom/alibaba/rocketmq/rocketmq-console/build/gitjar.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -x
+cd /opt/soft/version
+git clone https://e.coding.net/king019/github/rocketmq-externals.git
+cd rocketmq-externals
+cd rocketmq-console
+mvn versions:set -DnewVersion=release
+mvn clean install -DskipTests -Dmaven.javadoc.skip=true -Dgpg.skip -T 2
+
+
+cp target/rocketmq-console-ng-release.jar /opt/soft/rocketmq-console-ng-release.jar
+
+mvn clean
+rm -fr ~/.m2/repository

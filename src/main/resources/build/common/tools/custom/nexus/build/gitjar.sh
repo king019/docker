4ca7d5c742bd873c164063559a81696f14754604
k <k@qq.com>
2021-02-26 00:57:18 +0800

    1

---
+++ src/main/resources/build/common/tools/custom/nexus/build/gitjar.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -x
+cd /opt/soft/version
+git clone https://e.coding.net/king019/github/nexus-public.git
+cd nexus-public
+git checkout release-3.7.0-04
+mvn clean install -DskipTests -Dmaven.javadoc.skip=true -T 2
+cd /opt/soft/version/nexus-public/assemblies/nexus-base-template/target/nexus-base-template-3.7.0-04.zip /root/tools/nexus-base-template-3.7.0-04.zip
+mvn clean
+rm -fr ~/.m2/repository

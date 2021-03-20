91bdf9aa1646553f331c206285485bb888993ef6
k <k@qq.com>
2021-03-20 10:40:15 +0800

    1

--- src/main/resources/build/common/tools/custom/nexus/version/build/gitjar.sh
+++ src/main/resources/build/common/tools/custom/nexus/version/build/gitjar.sh
@@ -5,8 +5,10 @@ mkdir -p /opt/soft/version
 cd /opt/soft/version
 git clone https://e.coding.net/king019/github/nexus-public.git
 cd nexus-public
-git checkout release-3.7.0-04
+git checkout release-3.8.0-02
 mvn clean install -DskipTests -Dmaven.javadoc.skip=true -T 2
-cp /opt/soft/version/nexus-public/assemblies/nexus-base-template/target/nexus-base-template-3.7.0-04.zip /root/tools/nexus-base-template-3.7.0-04.zip
+cp /opt/soft/version/nexus-public/assemblies/nexus-base-template/target/nexus-base-template-3.8.0-02.zip /root/tools/nexus-base-template-3.8.0-02.zip
 mvn clean
 rm -fr ~/.m2/repository
+cd /root/tools/;unzip nexus-base-template-3.8.0-02.zip
+cd /root/tools/;mv nexus-base-template-3.8.0-02 nexus

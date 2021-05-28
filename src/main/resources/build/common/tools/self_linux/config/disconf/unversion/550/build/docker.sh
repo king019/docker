fc7cb1b6776d6b8f0aba7e2a2d93fc7af15269c1
k <k@qq.com>
2021-05-29 00:55:23 +0800

    1

--- src/main/resources/build/common/tools/self_linux/config/disconf/unversion/550/build/docker.sh
+++ src/main/resources/build/common/tools/self_linux/config/disconf/unversion/550/build/docker.sh
@@ -2,12 +2,7 @@
 set -x
 source /etc/profile
 
-cd /opt/soft/version/disconf
-ONLINE_CONFIG_PATH=/conf
-WAR_ROOT_PATH=/home/work/dsp/disconf-rd/war
-export ONLINE_CONFIG_PATH
-export WAR_ROOT_PATH
-cd disconf-web
-sh deploy/deploy.sh
 
+sh /opt/soft/tools/tomcat/bin/shutdown.sh
+sh /opt/soft/tools/tomcat/bin/startup.sh
 tail -f /docker.sh
--- src/main/resources/build/common/tools/self_linux/config/disconf/unversion/550/build/gitjar.sh
+++ src/main/resources/build/common/tools/self_linux/config/disconf/unversion/550/build/gitjar.sh
@@ -3,3 +3,10 @@ set -x
 mkdir -p /opt/soft/version
 cd /opt/soft/version
 git clone https://e.coding.net/king019/github/disconf.git
+cd /opt/soft/version/disconf
+ONLINE_CONFIG_PATH=/conf
+WAR_ROOT_PATH=/opt/soft/tools/tomcat/webapps/ROOT
+export ONLINE_CONFIG_PATH
+export WAR_ROOT_PATH
+cd disconf-web
+sh deploy/deploy.sh

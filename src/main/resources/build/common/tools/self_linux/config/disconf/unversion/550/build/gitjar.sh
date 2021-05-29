115e020a41d0da1e85555e7095ca8de529f8a6fa
k <k@qq.com>
2021-05-29 18:38:10 +0800

    1

--- src/main/resources/build/common/tools/self_linux/config/disconf/unversion/550/build/gitjar.sh
+++ src/main/resources/build/common/tools/self_linux/config/disconf/unversion/550/build/gitjar.sh
@@ -6,12 +6,11 @@ cd /opt/soft/version
 git clone https://e.coding.net/king019/github/disconf.git
 cd /opt/soft/version/disconf
 
-
 sed -i 's/<java.version>1.6<\/java.version>/<java.version>1.8<\/java.version>/' pom.xml
 sed -i 's/<source.version>1.6<\/source.version>/<source.version>1.8<\/source.version>/' pom.xml
 sed -i 's/<target.version>1.6<\/target.version>/<target.version>1.8<\/target.version>/' pom.xml
 
-
+cat pom.xml
 
 ONLINE_CONFIG_PATH=/conf
 WAR_ROOT_PATH=/opt/soft/tools/tomcat/webapps/ROOT

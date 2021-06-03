62e21f26352f7100eedc3dbf2b4a91d03d69da60
k <k@qq.com>
2021-06-04 01:31:35 +0800

    1

--- src/main/resources/build/common/tools/self_linux/alibaba/dubbo/consumer/unversion/550/build/gitjar.sh
+++ src/main/resources/build/common/tools/self_linux/alibaba/dubbo/consumer/unversion/550/build/gitjar.sh
@@ -4,7 +4,6 @@ mkdir -p /opt/soft/version
 cd /opt/soft/version
 git clone https://e.coding.net/king019/github/ali_fw_fw.git
 cd ali_fw_fw
-cd $(find . -name 'fw_rpc_dubbo_anno_api')
 mvn clean install -DskipTests -Dmaven.javadoc.skip=true -Dgpg.skip -T 2
 find . -name fw_rpc_dubbo_anno_api-1.0-SNAPSHOT.jar | awk '{print "cp " $1  " /opt/soft/fw_rpc_dubbo_anno_api-1.0-SNAPSHOT.jar"}' | sh
 mvn clean
--- src/main/resources/build/common/tools/self_linux/alibaba/dubbo/provider/unversion/550/build/gitjar.sh
+++ src/main/resources/build/common/tools/self_linux/alibaba/dubbo/provider/unversion/550/build/gitjar.sh
@@ -5,7 +5,6 @@ cd /opt/soft/version
 git clone https://e.coding.net/king019/github/ali_fw_fw.git
 
 cd ali_fw_fw
-cd $(find . -name 'fw_rpc_dubbo_anno')
 mvn clean install -DskipTests -Dmaven.javadoc.skip=true -Dgpg.skip -T 2
 find . -name fw_rpc_dubbo_anno_service-1.0-SNAPSHOT.jar | awk '{print "cp " $1  " /opt/soft/fw_rpc_dubbo_anno_service-1.0-SNAPSHOT.jar"}' | sh
 mvn clean

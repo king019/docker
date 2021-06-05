07f80a8f689317daa9aa4219ac7ea84aafd547d0
k <k@qq.com>
2021-06-05 21:49:50 +0800

    1

--- src/main/resources/build/common/tools/self_linux/alibaba/nacos/nacos/version/500/build/gitjar.sh
+++ src/main/resources/build/common/tools/self_linux/alibaba/nacos/nacos/version/500/build/gitjar.sh
@@ -4,7 +4,7 @@ mkdir -p /opt/soft/version
 cd /opt/soft/version
 git clone https://e.coding.net/king019/github/nacos.git
 cd nacos
-git checkout 1.4.1
+git checkout 2.0.1
 cd consistency
 sed -i 's/<pluginId>grpc-java<\/pluginId>/<pluginId>grpc-java<\/pluginId><skip>true<\/skip>/' pom.xml
 protoc --java_out src/main/java/ src/main/proto/consistency.proto
@@ -14,7 +14,7 @@ cd ..
 
 mvn -Prelease-nacos -Dmaven.test.skip=true clean install -U  -T 2
 
-find . -name nacos-server-1.4.1.zip | awk '{print "cp " $1  " /opt/soft/nacos-server-1.4.1.zip"}' | sh
+find . -name nacos-server-2.0.1.zip | awk '{print "cp " $1  " /opt/soft/nacos-server-2.0.1.zip"}' | sh
 mvn clean
 rm -fr ~/.m2/repository
 

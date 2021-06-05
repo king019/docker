9f7a9a5f6c6b2889a1479037d7424270a195200e
k <k@qq.com>
2021-06-06 00:51:21 +0800

    1

---
+++ src/main/resources/build/common/tools/self_linux/alibaba/arthas/version/500/build/gitjar.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -x
+mkdir -p /opt/soft/version
+cd /opt/soft/version
+git clone https://e.coding.net/king019/github/arthas.git
+cd arthas
+git checkout arthas-all-3.5.0
+mvn clean package -DskipTests -Dmaven.javadoc.skip=true -Dgpg.skip -T 2
+find . -name arthas-boot-jar-with-dependencies.jar | awk '{print "cp " $1  " /opt/soft/arthas-boot.jar"}' | sh
+mvn clean
+rm -fr ~/.m2/repository

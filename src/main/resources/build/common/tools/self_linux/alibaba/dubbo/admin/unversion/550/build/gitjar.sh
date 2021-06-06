553bf7a240644519b5a3faeae74d6b09e3d59147
k <k@qq.com>
2021-06-07 02:21:37 +0800

    1

--- src/main/resources/build/common/tools/self_linux/alibaba/dubbo/admin/unversion/550/build/gitjar.sh
+++ src/main/resources/build/common/tools/self_linux/alibaba/dubbo/admin/unversion/550/build/gitjar.sh
@@ -4,11 +4,26 @@ mkdir -p /opt/soft/version
 cd /opt/soft/version
 git clone https://e.coding.net/king019/github/dubbo-admin.git
 cd dubbo-admin
-git checkout master
+git checkout 0.2.0
+cd dubbo-admin-ui
+
+sed -i '38,80d' pom.xml
+npm install
+npm run build
+cd ..
 mvn versions:set -DnewVersion=release
 mvn clean install -DskipTests -Dmaven.javadoc.skip=true -Dgpg.skip -T 2
 
-cp dubbo-admin/target/dubbo-admin-release.jar /opt/soft/dubbo-admin-release.jar
-
+cp dubbo-admin-distribution/target/dubbo-admin-distribution-release.jar /opt/soft/dubbo-admin-distribution-release.jar
 mvn clean
 rm -fr ~/.m2/repository
+
+#cd dubbo-admin
+#git checkout master
+#mvn versions:set -DnewVersion=release
+#mvn clean install -DskipTests -Dmaven.javadoc.skip=true -Dgpg.skip -T 2
+#
+#cp dubbo-admin/target/dubbo-admin-release.jar /opt/soft/dubbo-admin-release.jar
+#
+#mvn clean
+#rm -fr ~/.m2/repository

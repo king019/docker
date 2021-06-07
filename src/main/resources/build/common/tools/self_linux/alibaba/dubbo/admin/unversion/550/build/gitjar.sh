8b4c37435368bdc37b8323ce9c012c460cd8a80a
k <k@qq.com>
2021-06-08 01:56:46 +0800

    1

--- src/main/resources/build/common/tools/self_linux/alibaba/dubbo/admin/unversion/550/build/gitjar.sh
+++ src/main/resources/build/common/tools/self_linux/alibaba/dubbo/admin/unversion/550/build/gitjar.sh
@@ -3,27 +3,30 @@ set -x
 mkdir -p /opt/soft/version
 cd /opt/soft/version
 git clone https://e.coding.net/king019/github/dubbo-admin.git
+
 cd dubbo-admin
-git checkout 0.2.0
-cd dubbo-admin-ui
-
-sed -i '38,80d' pom.xml
-npm install
-npm run build
-cd ..
-mvn versions:set -DnewVersion=release
+git checkout master
+cd dubbo-admin
 mvn clean install -DskipTests -Dmaven.javadoc.skip=true -Dgpg.skip -T 2
+cp target/dubbo-admin-0.0.1-SNAPSHOT.jar /opt/soft/dubbo-admin-release.jar
 
-cp dubbo-admin-distribution/target/dubbo-admin-release.jar /opt/soft/dubbo-admin-release.jar
 mvn clean
 rm -fr ~/.m2/repository
 
 #cd dubbo-admin
-#git checkout master
 #mvn versions:set -DnewVersion=release
+#
+#sed -i "s/@SPI(\"zookeeper\")/@SPI(\"nacos\")/g" dubbo-admin-server/src/main/java/org/apache/dubbo/admin/registry/config/GovernanceConfiguration.java
+#sed -i "s/@SPI(\"zookeeper\")/@SPI(\"nacos\")/g" dubbo-admin-server/src/main/java/org/apache/dubbo/admin/registry/metadata/MetaDataCollector.java
+#
+#cd dubbo-admin-ui
+#
+#sed -i '38,80d' pom.xml
+#npm install
+#npm run build
+#cd ..
 #mvn clean install -DskipTests -Dmaven.javadoc.skip=true -Dgpg.skip -T 2
 #
-#cp dubbo-admin/target/dubbo-admin-release.jar /opt/soft/dubbo-admin-release.jar
-#
+#cp dubbo-admin-distribution/target/dubbo-admin-release.jar /opt/soft/dubbo-admin-release.jar
 #mvn clean
 #rm -fr ~/.m2/repository

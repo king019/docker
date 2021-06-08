1f9002fa845f1137b9bbcba8abd0c9518635d6a5
k <k@qq.com>
2021-06-09 00:18:06 +0800

    1

--- src/main/resources/build/common/tools/self_linux/alibaba/dubbo/monitor/unversion/550/build/gitjar.sh
+++ src/main/resources/build/common/tools/self_linux/alibaba/dubbo/monitor/unversion/550/build/gitjar.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -x
+source /etc/profile
 mkdir -p /opt/soft/version
 cd /opt/soft/version
 git clone https://e.coding.net/king019/github/dubbo-admin.git
@@ -9,7 +10,9 @@ mvn versions:set -DnewVersion=release
 mvn clean install -DskipTests -Dmaven.javadoc.skip=true -Dgpg.skip -T 2
 
 cp dubbo-monitor-simple/target/dubbo-monitor-simple-release-assembly.tar.gz /opt/soft/dubbo-monitor-simple-release-assembly.tar.gz
-cd /opt/soft; tar -xzf dubbo-monitor-simple-release-assembly.tar.gz
-cd /opt/soft; mv dubbo-monitor-simple-release monitor
+cd /opt/soft
+tar -xzf dubbo-monitor-simple-release-assembly.tar.gz
+cd /opt/soft
+mv dubbo-monitor-simple-release monitor
 mvn clean
 rm -fr ~/.m2/repository

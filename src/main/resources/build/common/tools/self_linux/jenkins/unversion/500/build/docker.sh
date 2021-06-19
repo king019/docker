666072dd96346f2ecc89e5be6150c1957a932d67
k <k@qq.com>
2021-06-19 23:25:20 +0800

    1

--- src/main/resources/build/common/tools/self_linux/jenkins/unversion/500/build/docker.sh
+++ src/main/resources/build/common/tools/self_linux/jenkins/unversion/500/build/docker.sh
@@ -3,7 +3,6 @@ set -x
 source /etc/profile
 
 cat /root/.docker/config.json >/root/.jenkins/config.json
-
-sh /opt/soft/tomcat/bin/shutdown.sh
-sh /opt/soft/tomcat/bin/startup.sh
+sh /opt/soft/tools/tomcat/bin/shutdown.sh
+sh /opt/soft/tools/tomcat/bin/startup.sh
 tail -f /docker.sh
--- src/main/resources/build/common/tools/self_linux/jenkins/unversion/500/build/gitjar.sh
+++ src/main/resources/build/common/tools/self_linux/jenkins/unversion/500/build/gitjar.sh
@@ -9,6 +9,6 @@ cd war
 mvn clean war:war -DskipTests -Dmaven.javadoc.skip=true -Dgpg.skip -T 2
 
 cp target/jenkins.war /opt/soft/tools/jenkins.war
-
+cp /opt/soft/tools/jenkins.war /opt/soft/tools/tomcat/webapps/ROOT.war
 mvn clean
 rm -fr ~/.m2/repository

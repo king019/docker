ce68c9ec310a01c725e797cab1ba0c0df2e94fc4
k <k@qq.com>
2021-07-21 12:32:56 +0800

    1

--- src/main/resources/build/common/linux/proto_multi_version/centos/multi/100/build/run/protoc.sh
+++ src/main/resources/build/common/linux/proto_multi_version/centos/multi/100/build/run/protoc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
 dnf install -y autoconf automake libtool curl make gcc-c++ unzip
-mkdir -p /opt/soft/compile/protobuf
-cd /opt/soft/compile/protobuf
+mkdir -p /opt/soft/compile
+cd /opt/soft/compile
 
 git clone https://e.coding.net/king019/github/protobuf.git
 cd protobuf

61e90dfe5978366cb0877def10307409ffe60140
k <k@qq.com>
2021-08-14 17:21:30 +0800

    1

--- src/main/resources/build/common/tools/self_linux/alibaba/arthas/version/500/build/gitjar.sh
+++ src/main/resources/build/common/tools/self_linux/alibaba/arthas/version/500/build/gitjar.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -x
+source /etc/profile
 mkdir -p /opt/soft/version
 cd /opt/soft/version
 git clone https://e.coding.net/king019/github/arthas.git
--- src/main/resources/build/common/tools/self_linux/page/king019.github.io/unversion/550/build/gitjar.sh
+++ src/main/resources/build/common/tools/self_linux/page/king019.github.io/unversion/550/build/gitjar.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -x
+source /etc/profile
 mkdir -p /opt/soft/version
 cd /opt/soft/version
 git clone http://gitea:3001/king019/ali_fw_blog.git

da685445a23715b821c01fc25db9318f18c0a93c
k <k@qq.com>
2022-01-23 14:13:56 +0800

    1

--- src/main/resources/build/common/tools/self_alpine/alibaba/nacos/version/550/build/gitjar.sh
+++ src/main/resources/build/common/tools/self_alpine/alibaba/nacos/version/550/build/gitjar.sh
@@ -10,9 +10,12 @@ cd consistency
 sed -i 's/<pluginId>grpc-java<\/pluginId>/<pluginId>grpc-java<\/pluginId><skip>true<\/skip>/' pom.xml
 protoc --java_out src/main/java/ src/main/proto/consistency.proto
 protoc --java_out src/main/java/ src/main/proto/Data.proto
-
 cd ..
 
+cd console
+sed -i 's/<artifactId>spring-boot-maven-plugin<\/artifactId>/<artifactId>spring-boot-maven-plugin<\/artifactId><version>2.6.3<\/version>/' pom.xml
+
+cd ..
 mvn -Prelease-nacos -Dmaven.test.skip=true clean install
 
 find . -name nacos-server-2.0.0.zip | awk '{print "cp " $1  " /opt/soft/nacos-server-2.0.0.zip"}' | sh

68e91bac37e45eba3f72123c3c2c2df724f245a5
k <k@qq.com>
2022-02-12 20:13:48 +0800

    1

---
+++ src/main/resources/build/common/tools/self_linux/jdk/jdk17/version/550/build/docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -x
+source /etc/profile;java -version
+tail -f /docker.sh
---
+++ src/main/resources/build/common/tools/self_rhel/tunasync/version/550/build/tun_sync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+NowPlatform=$(uname -m)
+X86='x86_64'
+Arm='aarch64'
+if [[ $NowPlatform == $X86 ]]
+then
+  tar -xzf ./tunasync-linux-amd64-bin.tar.gz
+else [[ $NowPlatform == $Arm ]]
+  tar -xzf ./tunasync-linux-arm64-bin.tar.gz
+fi

02cc7a0a611140bdbe226ff096474baceff9d9e5
k <k@qq.com>
2022-10-18 12:15:37 +0800

    1

---
+++ src/main/resources/build/common/tools/self_linux/alibaba/sentinel/dashbord/1.4.2/550/build/docker.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -x
+source /etc/profile;java -version
+cd /opt/soft
+java $JAVA_OPTS -Dserver.port=8080 -Dcsp.sentinel.dashboard.server=localhost:8080 -Dproject.name=dashboard -jar sentinel-dashboard.jar
+tail -f /docker.sh
---
+++ src/main/resources/build/common/tools/self_linux/alibaba/sentinel/dashbord/1.6.3/550/build/gitjar.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -x
+source /etc/profile;java -version
+mkdir -p /opt/soft/version
+cd /opt/soft/version
+git clone https://gitee.com/mirrors/Sentinel.git
+cd Sentinel/sentinel-dashboard
+git checkout 1.6.3
+mvn clean install -DskipTests -Dmaven.javadoc.skip=true -Dgpg.skip
+find . -name sentinel-dashboard.jar | awk '{print "cp " $1  " /opt/soft/sentinel-dashboard.jar"}' | sh
+ls /opt/soft
+mvn clean
+/mvnclean.sh

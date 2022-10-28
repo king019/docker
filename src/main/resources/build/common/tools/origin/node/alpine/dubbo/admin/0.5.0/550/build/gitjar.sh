ca6f75b84cc7146680177bbcde117ca18d81854f
k <k@qq.com>
2022-10-29 00:21:17 +0800

    1

--- src/main/resources/build/common/tools/origin/node/alpine/dubbo/admin/0.5.0/550/build/gitjar.sh
+++ src/main/resources/build/common/tools/origin/node/alpine/dubbo/admin/0.5.0/550/build/gitjar.sh
@@ -10,18 +10,24 @@ source /etc/profile;java -version
 
 mkdir -p /opt/soft/version
 cd /opt/soft/version
-git clone https://e.coding.net/king019/github/dubbo-admin.git
+git clone https://gitee.com/king019/dubbo-admin.git
 
 cd dubbo-admin
-git checkout 0.4.0
-mvn clean package -Dmaven.test.skip=true -pl dubbo-admin-server
-cp dubbo-admin-server/target/dubbo-admin-server-0.4.0.jar /opt/soft/dubbo-admin-server.jar
+git checkout 0.5.0
 
 
 cd dubbo-admin-ui
 sed -i "s/port: 8082,/port: 8082,disableHostCheck: true,/g" ./vue.config.js
 npm install -g @vue/cli
 npm install
+
+
+cd ..
+mvn clean package -Dmaven.test.skip=true -pl dubbo-admin-server
+cp dubbo-admin-server/target/dubbo-admin-server-0.5.0.jar /opt/soft/dubbo-admin-server.jar
+
+
+
 
 
 cd /opt/soft/version

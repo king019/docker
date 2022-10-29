5269c29514242a798cb5a300e738a4752d626b86
k <k@qq.com>
2022-10-29 15:56:47 +0800

    1

--- src/main/resources/build/common/tools/self_linux/alibaba/sentinel/dashbord/version/550/build/gitjar.sh
+++ src/main/resources/build/common/tools/self_linux/alibaba/sentinel/dashbord/version/550/build/gitjar.sh
@@ -5,7 +5,7 @@ mkdir -p /opt/soft/version
 cd /opt/soft/version
 git clone https://gitee.com/mirrors/Sentinel.git sentinel
 cd sentinel/sentinel-dashboard
-git checkout 1.8.5
+git checkout 1.8.6
 mvn clean install -DskipTests -Dmaven.javadoc.skip=true -Dgpg.skip
 find . -name sentinel-dashboard.jar | awk '{print "cp " $1  " /opt/soft/sentinel-dashboard.jar"}' | sh
 ls /opt/soft

59e5071c140387cdb5f14f8a74a2077be37db41d
king019 <king019@users.noreply.github.com>
2023-02-27 15:03:01 +0000

    Update trans_nexus.sh

--- src/main/resources/build/common/linux/base_version/alpine_version/proto_version/alpine/3/50/build/trans_nexus.sh
+++ src/main/resources/build/common/linux/base_version/alpine_version/proto_version/alpine/3/50/build/trans_nexus.sh
@@ -2,5 +2,5 @@
 set -x
 
 
-sed -i 's/http/https/g' /etc/apk/repositories
+sed -i 's/https/http/g' /etc/apk/repositories
 sed -i 's/dl-cdn.alpinelinux.org/nexus:8081\/repository\/alpine/g' /etc/apk/repositories

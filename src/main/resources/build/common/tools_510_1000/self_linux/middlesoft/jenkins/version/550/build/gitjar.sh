e7b6fb10e6a2fbe3d68dce276ce21d47f643cca4
k <k@qq.com>
2024-12-29 14:13:01 +0800

    1

--- src/main/resources/build/common/tools_510_1000/self_linux/middlesoft/jenkins/version/550/build/gitjar.sh
+++ src/main/resources/build/common/tools_510_1000/self_linux/middlesoft/jenkins/version/550/build/gitjar.sh
@@ -6,8 +6,8 @@ source /etc/profile;java -version
 mkdir -p /opt/soft/version
 cd /opt/soft/version
 
-wget https://repo.huaweicloud.com/jenkins/war/2.471/jenkins.war
-#wget https://repo.huaweicloud.com/jenkins/war/latest/jenkins.war
+wget http://repo.huaweicloud.com/jenkins/war/2.471/jenkins.war
+
 
 cp jenkins.war /opt/soft/tool/jenkins.war
 ls /opt/soft/tool
